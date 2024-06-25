dd9d4bbf50111f7368f6a1ca7f0e9f249d6f408d
Vlad <enderbroshka225@gmail.com>
2024-06-25 20:02:27 +0300

    feat: added comment about compile time

--- GeometryBox/Managers/FigureCalculator.cs
+++ GeometryBox/Managers/FigureCalculator.cs
@@ -8,6 +8,13 @@ namespace GeometryBox.Managers
         /// Used to get shape square in compile time without knowing which shape it is
         /// </summary>
         /// <param name="shape"></param>
+        /*
+         * Насколько я понимаю данное решение реализует определение площади в рамках compile-time,
+         * так как не требуется знать фигуру, а только что она реализует интерфейс фигуры.
+         * Если же реализовывать для run-time, то надо было бы делать алгоритм
+         * определения какая фигура задается по введеному количеству параметров, 
+         * что требует договоренностей, так как 1 значение может быть как кругом, так и квадратом.
+         */
         public double GetShapeSquare(IShape shape)
         {
             return shape.GetArea();
